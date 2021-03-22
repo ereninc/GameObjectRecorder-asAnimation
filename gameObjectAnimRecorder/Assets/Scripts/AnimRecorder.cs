54e3d8967defa6097fcf89d82fa9a8024108b53d
Eren Inanci <erencan_inanci@hotmail.com>
2021-03-23 00:15:18 +0300

    Scene 2 added. Player movement saved.

---
+++ gameObjectAnimRecorder/Assets/Scripts/AnimRecorder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+public class AnimRecorder : MonoBehaviour
+{
+    [SerializeField] private AnimationClip _animClip;
+    [SerializeField] private bool _recordAnim = false;
+    private GameObjectRecorder _gameObjectRecorder;
+
+    void Start()
+    {
+        _gameObjectRecorder = new GameObjectRecorder(gameObject);
+        _gameObjectRecorder.BindComponentsOfType<Transform>(gameObject, true);
+    }
+
+    void LateUpdate()
+    {
+        if (_animClip == null)
+        {
+            return;
+        }
+        _gameObjectRecorder.TakeSnapshot(Time.deltaTime);
+    }
+
+    private void OnDisable()
+    {
+        if (_animClip == null)
+        {
+            return;
+        }
+        if (_gameObjectRecorder.isRecording)
+        {
+            _gameObjectRecorder.SaveToClip(_animClip);
+        }
+    }
+}
---
+++ gameObjectAnimRecorder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _playerSpeed = 3f;
+
+    void FixedUpdate()
+    {
+        transform.Translate(0, 0, 1 * Time.deltaTime * _playerSpeed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0, -90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0, 90, 0);
+        }
+    }
+}
