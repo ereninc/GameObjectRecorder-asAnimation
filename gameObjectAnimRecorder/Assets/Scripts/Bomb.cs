cc753d31055ca942d3abfab37bbf1ee6cd6739ba
Eren Inanci <erencan_inanci@hotmail.com>
2021-03-22 23:48:47 +0300

    AnimRecorder - Finished.

---
+++ gameObjectAnimRecorder/Assets/Scripts/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float _explosionForce = 700f;
+
+    private void Explode()
+    {
+        var rigidbodies = FindObjectsOfType<Rigidbody>();
+        foreach (var rigidbody in rigidbodies)
+        {
+            rigidbody.AddExplosionForce(_explosionForce, transform.position, 10f);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Explode();
+        }
+    }
+}
